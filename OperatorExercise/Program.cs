5b005a8ba181e1d073256ff0177e8843589fdab2
Grayson Owen <graysonowen@Graysons-MBP.attlocal.net>
2020-06-22 08:54:14 -0500

    OperatorExercise

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+          //----------------Exercise 1-------------------
+
+           
+            var a = 17;
+            var b = 4;
+
+            var div = a / b;
+            var mod = a % b;
+
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+
+            //------------ Exercise 2------------------------
+
+            var r = 20;
+            var pi = Math.PI;
+
+            var areaOfCircle = pi * (r * r);
+
+            Console.WriteLine($"What is the radius of your circle? {r}");
+
+            Console.WriteLine($"The area of a circle with radius of {r} is {areaOfCircle}");
+        }
+    }
+}           
